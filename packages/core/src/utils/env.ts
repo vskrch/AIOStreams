4158ff6301f7575fb033f67b9353f0953579ad09
Venkata Sai Krishna chirasani <vskrch@gmail.com>
2025-07-04 22:50:40 -0400

    feat: add stream cache auto-refresh

--- packages/core/src/utils/env.ts
+++ packages/core/src/utils/env.ts
@@ -339,6 +339,10 @@ export const Env = cleanEnv(process.env, {
     default: -1,
     desc: 'Cache TTL for stream files. If -1, no caching will be done.',
   }),
+  STREAM_CACHE_REFRESH_COUNT: num({
+    default: 0,
+    desc: 'Number of times to refresh stream cache when it expires',
+  }),
   CATALOG_CACHE_TTL: num({
     default: 300,
     desc: 'Cache TTL for catalog files',
