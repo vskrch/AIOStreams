c77dcd82f533b3eceb229b0d47f6bdd54ecda847
Viren070 <viren070@protonmail.com>
2025-09-13 22:23:36 +0100

    fix: allow any string for URL

--- packages/core/src/db/schemas.ts
+++ packages/core/src/db/schemas.ts
@@ -546,7 +546,7 @@ export type Manifest = z.infer<typeof ManifestSchema>;
 export const SubtitleSchema = z
   .object({
     id: z.string().min(1),
-    url: z.string().url(),
+    url: z.string(),
     lang: z.string().min(1),
   })
   .passthrough();
@@ -559,7 +559,7 @@ export type Subtitle = z.infer<typeof SubtitleSchema>;
 
 export const StreamSchema = z
   .object({
-    url: z.string().url().or(z.null()).optional(),
+    url: z.string().or(z.null()).optional(),
     ytId: z.string().min(1).or(z.null()).optional(),
     infoHash: z.string().min(1).or(z.null()).optional(),
     fileIdx: z.number().or(z.null()).optional(),
@@ -658,7 +658,7 @@ export const ParsedStreamSchema = z.object({
     .optional(),
   duration: z.number().optional(),
   library: z.boolean().optional(),
-  url: z.string().url().optional(),
+  url: z.string().optional(),
   ytId: z.string().min(1).optional(),
   externalUrl: z.string().min(1).optional(),
   error: z
