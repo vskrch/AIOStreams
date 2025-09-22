b3549cd989a9f32c42fec7bdc39254cbc68a6451
Viren070 <viren070@protonmail.com>
2025-09-22 16:10:40 +0100

    fix: adjust bluray remux regex

--- packages/core/src/parser/regex.ts
+++ packages/core/src/parser/regex.ts
@@ -59,8 +59,7 @@ export const PARSE_REGEX: PARSE_REGEX = {
     '144p': createRegex('(bd|hd|m)?(144(p|i)?)'),
   },
   qualities: {
-    'BluRay REMUX':
-      /(remux.*blu[\s.\-_]?ray|blu[\s.\-_]?ray.*remux|(bd|br|b|uhd)[\s.\-_]?remux)/i,
+    'BluRay REMUX': createRegex('(bd|br|b|uhd)?remux'),
     BluRay: createRegex(
       'blu[ .\\-_]?ray|((bd|br|b)[ .\\-_]?(rip|r)?)(?![ .\\-_]?remux)'
     ),
