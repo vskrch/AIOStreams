bd0531388897767507fad9265b2a8a9f0b059554
Viren070 <viren070@protonmail.com>
2025-09-26 22:27:17 +0100

    fix(debrid/utils): allow season 1 with correct absolute episode to pass validation

--- packages/core/src/debrid/utils.ts
+++ packages/core/src/debrid/utils.ts
@@ -65,14 +65,24 @@ export interface NZBWithSelectedFile extends NZB {
 
 // helpers
 export const isSeasonWrong = (
-  parsed: { seasons?: number[] },
-  metadata?: { season?: number }
+  parsed: { seasons?: number[]; episodes?: number[] },
+  metadata?: { season?: number; absoluteEpisode?: number }
 ) => {
   if (
     parsed.seasons?.length &&
     metadata?.season &&
     !parsed.seasons.includes(metadata.season)
   ) {
+    // allow if season is "wrong" with value of 1 but absolute episode is correct
+    if (
+      parsed.seasons.length === 1 &&
+      parsed.seasons[0] === 1 &&
+      parsed.episodes?.length &&
+      metadata.absoluteEpisode &&
+      parsed.episodes.includes(metadata.absoluteEpisode)
+    ) {
+      return false;
+    }
     return true;
   }
   return false;
